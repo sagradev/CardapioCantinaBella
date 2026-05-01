4599c9e75913d57090283901f082db189defe146
Gustavo Rodrigues Gutierrez <mmpguty.5@gmail.com>
2026-05-01 19:50:14 -0400

    Mais uma tentativa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";  // ← linha nova
+import vercel from "@astrojs/vercel/serverless";
+
 
 export default defineConfig({
   output: "server",
