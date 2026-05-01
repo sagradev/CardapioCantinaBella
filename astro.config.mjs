be2a86b294645922536dfc57a64a25ca4d2b84b2
Gustavo Rodrigues Gutierrez <mmpguty.5@gmail.com>
2026-05-01 19:39:24 -0400

    Ajustado npm do adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless";  // ← linha nova
 
 export default defineConfig({
   output: "server",
-  adapter: vercel(),        // ← era node()
+  adapter: vercel(),           // ← era node(...)
   integrations: [react(), tailwind()],
+  server: { port: 4321 },
 });
